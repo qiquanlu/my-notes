e4360167acd7d9471bdc8980a626f19a93aee637
Qiquan Lu <kejeon@gmail.com>
2022-04-30 21:44:51 -0400

    markdown show linenumber

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,23 +6,13 @@ export default defineConfig({
     description: "Just some of my notes",
     lastUpdated: true,
     themeConfig: {
-        // nav: [
-        //     { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
-        //     {
-        //         text: 'Config Reference',
-        //         link: '/config/basics',
-        //         activeMatch: '^/config/'
-        //     },
-        //     {
-        //         text: 'Release Notes',
-        //         link: 'https://github.com/vuejs/vitepress/releases'
-        //     }
-        // ],
-
         sidebar: {
             '/graph/': getAlgoSidebar(),
             '/': getAlgoSidebar()
         }
+    },
+    markdown: {
+        lineNumbers: true
     }
 })
 
